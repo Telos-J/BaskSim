95e564f894b681ded170d3eaaf2fb26623240666
25ndy <25ndyh@gmail.com>
2020-12-19 14:48:13 +0900

    Purple players created.

--- javascript/js/app.js
+++ javascript/js/app.js
@@ -1,11 +1,11 @@
 class Attribute {
-    constructor(shoot, shoot3, defence, stamina, speed) {
-        this.shoot = shoot;
-        this.shoot3 = shoot3;
-        this.defence = defence;
-        this.stamina = stamina;
-        this.speed = speed;
-    }
+  constructor(shoot, shoot3, defence, stamina, speed) {
+    this.shoot = shoot;
+    this.shoot3 = shoot3;
+    this.defence = defence;
+    this.stamina = stamina;
+    this.speed = speed;
+  }
 }
 
 const centerAttribute = new Attribute(0.45, 0.24, 0.67, 89, 65);
@@ -15,205 +15,199 @@ const pguardAttribute = new Attribute(0.58, 0.39, 0.85, 71, 83);
 const sguardAttribute = new Attribute(0.61, 0.48, 0.79, 90, 83);
 
 class Player {
-    constructor(
-        role,
-        height,
-        weight,
-        back_number,
-        name,
-        x,
-        y,
-        target,
-        attribute
-    ) {
-        this.role = role;
-        this.height = height;
-        this.weight = weight;
-        this.back_number = back_number;
-        this.name = name;
-        this.x = x;
-        this.y = y;
-        this.target = target;
-        this.hasBall = false;
-        this.attribute = attribute;
-        this.attempt = 0;
-        this.score = 0;
-        this.goals = 0;
-        this.shootProb = 0;
-        this.frames = [];
-        this.framenum = 0;
-        this.isMoving = false;
-        this.wasMoving = false;
-        this.playerDOM = undefined;
-    }
+  constructor(
+    role,
+    height,
+    weight,
+    back_number,
+    name,
+    x,
+    y,
+    target,
+    attribute
+  ) {
+    this.role = role;
+    this.height = height;
+    this.weight = weight;
+    this.back_number = back_number;
+    this.name = name;
+    this.x = x;
+    this.y = y;
+    this.target = target;
+    this.hasBall = false;
+    this.attribute = attribute;
+    this.attempt = 0;
+    this.score = 0;
+    this.goals = 0;
+    this.shootProb = 0;
+    this.frames = [];
+    this.framenum = 0;
+    this.isMoving = false;
+    this.wasMoving = false;
+    this.playerDOM = undefined;
+  }
 
-    grabBall(ball) {
-        const distance = Math.hypot(this.x - ball.x, this.y - ball.y);
-        if (distance < ball.size && !ball.shooting) {
-            this.hasBall = true;
-            ball.x = this.x;
-            ball.y = this.y;
-        }
+  grabBall(ball) {
+    const distance = Math.hypot(this.x - ball.x, this.y - ball.y);
+    if (distance < ball.size && !ball.shooting) {
+      this.hasBall = true;
+      ball.x = this.x;
+      ball.y = this.y;
     }
-    shoot(ball) {
-        this.hasBall = false;
-        ball.shooting = true;
-        ball.target.x = this.target.x;
-        ball.target.y = this.target.y;
-        const distHoop = Math.hypot(
-            this.x - this.target.x,
-            this.y - this.target.y
-        );
-        if (distHoop > 235.8) ball.probability = this.attribute.shoot3;
-        else ball.probability = this.attribute.shoot;
-    }
-    updateStat({ goal = true, bball_probability }) {
-        this.attempt += 1;
-        if (goal) {
-            if (bball_probability == controlPlayer.attribute.shoot3)
-                this.score += 3;
-            else this.score += 2;
+  }
+  shoot(ball) {
+    this.hasBall = false;
+    ball.shooting = true;
+    ball.target.x = this.target.x;
+    ball.target.y = this.target.y;
+    const distHoop = Math.hypot(this.x - this.target.x, this.y - this.target.y);
+    if (distHoop > 235.8) ball.probability = this.attribute.shoot3;
+    else ball.probability = this.attribute.shoot;
+  }
+  updateStat({ goal = true, bball_probability }) {
+    this.attempt += 1;
+    if (goal) {
+      if (bball_probability == controlPlayer.attribute.shoot3) this.score += 3;
+      else this.score += 2;
 
-            this.goals += 1;
-        }
-        this.shootProb = this.goals / this.attempt;
-        console.log('score: ', this.score);
-        console.log('shootProb: ', this.shootProb);
+      this.goals += 1;
     }
+    this.shootProb = this.goals / this.attempt;
+    console.log("score: ", this.score);
+    console.log("shootProb: ", this.shootProb);
+  }
 }
 
 const bball = {
-    x: buffer.canvas.width / 2,
-    y: buffer.canvas.height / 2,
-    speed: 10,
-    size: 15,
-    shooting: false,
-    bouncingoff: false,
-    probability: 0,
-    target: { x: 0, y: 0 },
-    move() {
-        const dx = this.target.x - this.x;
-        const dy = this.target.y - this.y;
-        const angle = Math.atan2(dy, dx);
+  x: buffer.canvas.width / 2,
+  y: buffer.canvas.height / 2,
+  speed: 10,
+  size: 15,
+  shooting: false,
+  bouncingoff: false,
+  probability: 0,
+  target: { x: 0, y: 0 },
+  move() {
+    const dx = this.target.x - this.x;
+    const dy = this.target.y - this.y;
+    const angle = Math.atan2(dy, dx);
 
-        this.x += this.speed * Math.cos(angle);
-        this.y += this.speed * Math.sin(angle);
-    },
-    reachTarget() {
-        const dist = Math.hypot(this.target.x - this.x, this.target.y - this.y);
+    this.x += this.speed * Math.cos(angle);
+    this.y += this.speed * Math.sin(angle);
+  },
+  reachTarget() {
+    const dist = Math.hypot(this.target.x - this.x, this.target.y - this.y);
 
-        if (dist < this.size / 1.5) return true;
-        else return false;
-    },
-    isGoal() {
-        return Math.random() < bball.probability;
-    },
-    makeGoal() {
-        this.x = this.target.x;
-        this.y = this.target.y;
-    },
-    bounceoff() {
-        this.bouncingoff = true;
-        const angle = Math.random() * Math.PI + (Math.PI * 3) / 2;
-        this.target.x = this.target.x - 80 * Math.cos(angle);
-        this.target.y = this.target.y + 80 * Math.sin(angle);
-    },
+    if (dist < this.size / 1.5) return true;
+    else return false;
+  },
+  isGoal() {
+    return Math.random() < bball.probability;
+  },
+  makeGoal() {
+    this.x = this.target.x;
+    this.y = this.target.y;
+  },
+  bounceoff() {
+    this.bouncingoff = true;
+    const angle = Math.random() * Math.PI + (Math.PI * 3) / 2;
+    this.target.x = this.target.x - 80 * Math.cos(angle);
+    this.target.y = this.target.y + 80 * Math.sin(angle);
+  },
 };
 
 function update() {
-    controlPlayer.wasMoving = controlPlayer.isMoving;
+  controlPlayer.wasMoving = controlPlayer.isMoving;
 
-    if (buffer.canvas.height >= controlPlayer.y) {
-        if (controller.down.active) {
-            controlPlayer.y += 5;
-            controlPlayer.isMoving = true;
-        } else controlPlayer.isMoving = false;
+  if (buffer.canvas.height >= controlPlayer.y) {
+    if (controller.down.active) {
+      controlPlayer.y += 5;
+      controlPlayer.isMoving = true;
+    } else controlPlayer.isMoving = false;
+  }
+  if (0 < controlPlayer.y) {
+    if (controller.up.active) {
+      controlPlayer.y -= 5;
+      controlPlayer.isMoving = true;
     }
-    if (0 < controlPlayer.y) {
-        if (controller.up.active) {
-            controlPlayer.y -= 5;
-            controlPlayer.isMoving = true;
-        }
+  }
+  if (buffer.canvas.width >= controlPlayer.x) {
+    if (controller.right.active) {
+      controlPlayer.playerDOM.querySelector("svg").classList.remove("flip");
+      controlPlayer.x += 5;
+      controlPlayer.isMoving = true;
     }
-    if (buffer.canvas.width >= controlPlayer.x) {
-        if (controller.right.active) {
-            controlPlayer.x += 5;
-            controlPlayer.isMoving = true;
-        }
+  }
+  if (0 < controlPlayer.x) {
+    if (controller.left.active) {
+      controlPlayer.playerDOM.querySelector("svg").classList.add("flip");
+      controlPlayer.x -= 5;
+      controlPlayer.isMoving = true;
     }
-    if (0 < controlPlayer.x) {
-        if (controller.left.active) {
-            controlPlayer.x -= 5;
-            controlPlayer.isMoving = true;
-        }
-    }
-    if (controller.space.active && controlPlayer.hasBall)
-        controlPlayer.shoot(bball);
-    if (
-        !controller.left.active &&
-        !controller.down.active &&
-        !controller.up.active &&
-        !controller.right.active
-    )
-        controlPlayer.isMoving = false;
+  }
+  if (controller.space.active && controlPlayer.hasBall)
+    controlPlayer.shoot(bball);
+  if (
+    !controller.left.active &&
+    !controller.down.active &&
+    !controller.up.active &&
+    !controller.right.active
+  )
+    controlPlayer.isMoving = false;
 
-    if (!controlPlayer.wasMoving && controlPlayer.isMoving) {
-        const paths = Array.from(
-            controlPlayer.playerDOM.querySelectorAll('path')
-        );
-        walkAnimation(paths);
-    } else if (controlPlayer.wasMoving && !controlPlayer.isMoving) {
-        const paths = Array.from(
-            controlPlayer.playerDOM.querySelectorAll('path')
-        );
-        idleAnimation(paths);
+  if (!controlPlayer.wasMoving && controlPlayer.isMoving) {
+    const paths = Array.from(controlPlayer.playerDOM.querySelectorAll("path"));
+    walkAnimation(paths);
+  } else if (controlPlayer.wasMoving && !controlPlayer.isMoving) {
+    const paths = Array.from(controlPlayer.playerDOM.querySelectorAll("path"));
+    idleAnimation(paths);
+  }
+
+  controlPlayer.grabBall(bball);
+
+  if (bball.shooting) {
+    bball.move();
+    if (bball.reachTarget()) {
+      bball.shooting = false;
+
+      if (bball.isGoal()) {
+        bball.makeGoal();
+        controlPlayer.updateStat({
+          goal: true,
+          ball_probablity: bball.probability,
+        });
+      } else {
+        bball.bounceoff();
+        controlPlayer.updateStat({
+          goal: false,
+        });
+      }
     }
-
-    controlPlayer.grabBall(bball);
-
-    if (bball.shooting) {
-        bball.move();
-        if (bball.reachTarget()) {
-            bball.shooting = false;
-
-            if (bball.isGoal()) {
-                bball.makeGoal();
-                controlPlayer.updateStat({
-                    goal: true,
-                    ball_probablity: bball.probability,
-                });
-            } else {
-                bball.bounceoff();
-                controlPlayer.updateStat({
-                    goal: false,
-                });
-            }
-        }
-    } else if (bball.bouncingoff) {
-        bball.move();
-        if (bball.reachTarget()) {
-            bball.bouncingoff = false;
-        }
+  } else if (bball.bouncingoff) {
+    bball.move();
+    if (bball.reachTarget()) {
+      bball.bouncingoff = false;
     }
+  }
 }
 
 function render() {
-    drawBackground('#b86125');
-    drawCourtLines();
-    drawTeam(roster1, 'green');
-    // drawTeam(roster2, "purple");
-    draw_bball();
-    context.drawImage(
-        buffer.canvas,
-        0,
-        0,
-        buffer.canvas.width,
-        buffer.canvas.height,
-        0,
-        0,
-        context.canvas.width,
-        context.canvas.height
-    );
+  drawBackground("#b86125");
+  drawCourtLines();
+  drawTeam(roster1, "green");
+  drawTeam(roster2, "purple");
+  draw_bball();
+  context.drawImage(
+    buffer.canvas,
+    0,
+    0,
+    buffer.canvas.width,
+    buffer.canvas.height,
+    0,
+    0,
+    context.canvas.width,
+    context.canvas.height
+  );
 }
 
 const [roster1, roster2] = generateTeam();
--- javascript/js/setupApp.js
+++ javascript/js/setupApp.js
@@ -4,12 +4,12 @@ let made = 0;
 let shootProb = 0;
 
 function generateTeam() {
-    const target1 = {
-        x: buffer.canvas.width - 55,
-        y: buffer.canvas.height / 2,
-    };
-    // prettier-ignore
-    const team1 = [
+  const target1 = {
+    x: buffer.canvas.width - 55,
+    y: buffer.canvas.height / 2,
+  };
+  // prettier-ignore
+  const team1 = [
         new Player('PG', 185, 86, 11, 'Myron Boyce', 338, 272, target1,pguardAttribute),
         new Player('SG', 198, 94, 24, 'Adam Chester', 293, 135, target1,sguardAttribute),
         new Player('SF', 202, 101, 34, 'Daanyal Graves', 101, 423,sforwardAttribute),
@@ -17,68 +17,74 @@ function generateTeam() {
         new Player('C', 217, 121, 12, 'Donovan Robinson', 154, 341, target1,centerAttribute),
     ];
 
-    const team1DOM = document.querySelectorAll('.green');
-    team1.forEach((player, index) => {
-        player.playerDOM = team1DOM[index];
-        setDOM(player.playerDOM, player);
-    });
+  const team1DOM = document.querySelectorAll(".green");
+  team1.forEach((player, index) => {
+    player.playerDOM = team1DOM[index];
+    setDOM(player.playerDOM, player);
+  });
 
-    const target2 = { x: 55, y: buffer.canvas.height / 2 };
-    // prettier-ignore
-    const team2 = [
+  const target2 = { x: 55, y: buffer.canvas.height / 2 };
+  // prettier-ignore
+  const team2 = [
         new Player('PG', 184, 89, 7, 'Damion Lee', buffer.canvas.width - 338, 272, target2,pguardAttribute),
         new Player('SG', 200, 98, 13, 'Chris Paul', buffer.canvas.width - 293, 135, target2,sguardAttribute),
         new Player('SF', 206, 102, 23, 'Zaire Willams', buffer.canvas.width - 101, 423, target2,sforwardAttribute),
         new Player('PF', 210, 107, 34, 'Giannis Antetokumpo', buffer.canvas.width - 88, 181, target2,pforwardAttribute),
         new Player('C', 216, 128, 54, 'Dwight Howard', buffer.canvas.width - 154, 341, target2,centerAttribute),
     ];
+  const team2DOM = document.querySelectorAll(".purple");
+  team2.forEach((player, index) => {
+    player.playerDOM = team2DOM[index];
+    setDOM(player.playerDOM, player);
+  });
 
-    return [team1, team2];
+  return [team1, team2];
 }
 
 function setDOM(playerDOM, player) {
-    const displayDOM = document.querySelector('.display');
+  const displayDOM = document.querySelector(".display");
 
-    playerDOM.getElementsByClassName('number')[0].innerHTML =
-        player.back_number;
-    const blob = playerDOM.getElementsByClassName('blob')[0];
-    const blob_paths = Array.from(blob.children);
-    idleAnimation(blob_paths);
+  playerDOM.getElementsByClassName("number")[0].innerHTML = player.back_number;
+  const blob = playerDOM.getElementsByClassName("blob")[0];
+  const blob_paths = Array.from(blob.children);
+  idleAnimation(blob_paths);
 
-    playerDOM.addEventListener('mouseover', (e) => {
-        displayDOM.style.display = 'block';
-        displayDOM.style.transform =
-            'translate(' + (e.clientX + 20) + 'px, ' + (e.clientY + 20) + 'px)';
-        displayDOM.innerHTML =
-            player.name +
-            '<br>' +
-            player.role +
-            '<br>' +
-            player.height +
-            'cm<br>' +
-            player.weight +
-            'kg';
-    });
+  playerDOM.addEventListener("mouseover", (e) => {
+    displayDOM.style.display = "block";
+    displayDOM.style.transform =
+      "translate(" + (e.clientX + 20) + "px, " + (e.clientY + 20) + "px)";
+    displayDOM.innerHTML =
+      player.name +
+      "<br>" +
+      player.role +
+      "<br>" +
+      player.height +
+      "cm<br>" +
+      player.weight +
+      "kg";
+  });
 
-    playerDOM.addEventListener('click', () => {
-        controlPlayer = player;
-    });
+  playerDOM.addEventListener("click", () => {
+    controlPlayer = player;
+  });
 
-    playerDOM.addEventListener(
-        'mouseleave',
-        () => (displayDOM.style.display = 'none')
-    );
+  playerDOM.addEventListener(
+    "mouseleave",
+    () => (displayDOM.style.display = "none")
+  );
 }
 
 function drawTeam(team, color) {
-    const playerDOM = document.querySelectorAll('.' + color);
+  const playerDOM = document.querySelectorAll("." + color);
 
-    team.forEach((player, index) => {
-        let position = convertToWindowCoord(
-            new Vector2(player.x - 20, player.y - 20)
-        );
+  team.forEach((player, index) => {
+    let position = convertToWindowCoord(
+      player.playerDOM.querySelector("svg").classList.contains("flip")
+        ? new Vector2(player.x - 20, player.y - 20)
+        : new Vector2(player.x - 40, player.y - 20)
+    );
 
-        playerDOM[index].style.transform =
-            'translate(' + position.x + 'px, ' + position.y + 'px)';
-    });
+    playerDOM[index].style.transform =
+      "translate(" + position.x + "px, " + position.y + "px)";
+  });
 }
