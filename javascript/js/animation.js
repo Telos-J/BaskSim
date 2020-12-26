d5aa78826f13ab137b9e6ac6f762877c8379a129
25ndy <25ndyh@gmail.com>
2020-12-26 20:22:10 +0900

    Add ballDOM

--- javascript/js/animation.js
+++ javascript/js/animation.js
@@ -1,45 +1,62 @@
 function idleAnimation(paths) {
-    paths.map(function (item, idx) {
-        gsap.fromTo(
-            item,
-            {
-                morphSVG: idle_paths[idx],
-            },
-            {
-                duration: 1,
-                repeat: -1,
-                yoyo: true,
-                morphSVG: idle2_paths[idx],
-            }
-        );
-    });
+  paths.map(function (item, idx) {
+    gsap.fromTo(
+      item,
+      {
+        morphSVG: idle_paths[idx],
+      },
+      {
+        duration: 1,
+        repeat: -1,
+        yoyo: true,
+        morphSVG: idle2_paths[idx],
+      }
+    );
+  });
 }
 
 function walkAnimation(paths) {
-    paths.map(function (item, idx) {
-        if (idx == 0) {
-            shapeIndex = 10;
-        } else if (idx == 4 || idx == 1) {
-            shapeIndex = 0;
-        } else {
-            shapeIndex = 'auto';
+  paths.map(function (item, idx) {
+    if (idx == 0) {
+      shapeIndex = 10;
+    } else if (idx == 4 || idx == 1 || idx == 7) {
+      shapeIndex = 0;
+    } else {
+      shapeIndex = "auto";
+    }
+    const timeline = gsap.timeline();
+    timeline
+      .fromTo(
+        item,
+        {
+          morphSVG: walk_paths[idx],
+        },
+        {
+          duration: 0.2,
+          repeat: -1,
+          yoyo: true,
+          ease: "none",
+          morphSVG: {
+            shape: walk2_paths[idx],
+            shapeIndex: shapeIndex,
+          },
         }
-        gsap.fromTo(
-            item,
-            {
-                morphSVG: walk_paths[idx],
-            },
-            {
-                duration: 0.2,
-                repeat: -1,
-                yoyo: true,
-                ease: 'none',
-                morphSVG: {
-                    shape: walk2_paths[idx],
-                    shapeIndex: shapeIndex,
-                },
-            },
-            0
-        );
-    });
+      )
+      .fromTo(
+        item,
+        {
+          morphSVG: walk2_paths[idx],
+        },
+        {
+          duration: 0.2,
+          repeat: -1,
+          yoyo: true,
+          ease: "none",
+          morphSVG: {
+            shape: walk3_paths[idx],
+            shapeIndex: shapeIndex,
+          },
+        }
+      );
+  });
 }
--- javascript/js/setup.js
+++ javascript/js/setup.js
@@ -1,32 +1,33 @@
-const canvas = document.querySelector('canvas');
-const context = canvas.getContext('2d');
-const buffer = document.createElement('canvas').getContext('2d');
+const canvas = document.querySelector("canvas");
+const context = canvas.getContext("2d");
+const buffer = document.createElement("canvas").getContext("2d");
 const controller = new Controller();
 
 buffer.canvas.width = 940;
 buffer.canvas.height = 500;
 
-window.addEventListener('resize', resize);
+window.addEventListener("resize", resize);
 
 function keyDownUp(event) {
-    controller.keyDownUp(event.type, event.keyCode);
+  controller.keyDownUp(event.type, event.keyCode);
 }
 
-window.addEventListener('keydown', keyDownUp);
-window.addEventListener('keyup', keyDownUp);
-canvas.addEventListener('mousemove', handleMouseMove);
+window.addEventListener("keydown", keyDownUp);
+window.addEventListener("keyup", keyDownUp);
+canvas.addEventListener("mousemove", handleMouseMove);
 
 gsap.registerPlugin(MorphSVGPlugin);
 
-const blobs = Array.from(document.getElementsByClassName('blob'));
-const idle = document.querySelector('.blob');
+const blobs = Array.from(document.getElementsByClassName("blob"));
+const idle = document.querySelector(".blob");
 const idle_paths = Array.from(idle.children);
-const idle2 = document.querySelector('#idle2');
+const idle2 = document.querySelector("#idle2");
 const idle2_paths = Array.from(idle2.children);
 
-const walk = document.querySelector('#walk');
+const walk = document.querySelector("#walk");
 const walk_paths = Array.from(walk.children);
-const walk2 = document.querySelector('#walk2');
+const walk2 = document.querySelector("#walk2");
 const walk2_paths = Array.from(walk2.children);
-
+const walk3 = document.querySelector("#walk3");
+const walk3_paths = Array.from(walk3.children);
 const walk_timeline = gsap.timeline({ paused: true });
