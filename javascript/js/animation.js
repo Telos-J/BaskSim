c8ad2e67f809c2ac511c64b378e07319f60c2ef1
jiseungjung <roadrunner23@yonsei.ac.kr>
2020-12-26 20:24:11 +0900

    fix animation

--- javascript/js/animation.js
+++ javascript/js/animation.js
@@ -1,62 +1,52 @@
 function idleAnimation(paths) {
-  paths.map(function (item, idx) {
-    gsap.fromTo(
-      item,
-      {
-        morphSVG: idle_paths[idx],
-      },
-      {
-        duration: 1,
-        repeat: -1,
-        yoyo: true,
-        morphSVG: idle2_paths[idx],
-      }
-    );
-  });
+    paths.map(function (item, idx) {
+        gsap.fromTo(
+            item,
+            {
+                morphSVG: idle_paths[idx],
+            },
+            {
+                duration: 1,
+                repeat: -1,
+                yoyo: true,
+                morphSVG: idle2_paths[idx],
+            }
+        );
+    });
 }
 
 function walkAnimation(paths) {
-  paths.map(function (item, idx) {
-    if (idx == 0) {
-      shapeIndex = 10;
-    } else if (idx == 4 || idx == 1 || idx == 7) {
-      shapeIndex = 0;
-    } else {
-      shapeIndex = "auto";
-    }
-    const timeline = gsap.timeline();
-    timeline
-      .fromTo(
-        item,
-        {
-          morphSVG: walk_paths[idx],
-        },
-        {
-          duration: 0.2,
-          repeat: -1,
-          yoyo: true,
-          ease: "none",
-          morphSVG: {
-            shape: walk2_paths[idx],
-            shapeIndex: shapeIndex,
-          },
+    paths.map(function (item, idx) {
+        if (idx == 0) {
+            shapeIndex = 10;
+        } else if (idx == 4 || idx == 1 || idx == 7) {
+            shapeIndex = 0;
+        } else {
+            shapeIndex = 'auto';
         }
-      )
-      .fromTo(
-        item,
-        {
-          morphSVG: walk2_paths[idx],
-        },
-        {
-          duration: 0.2,
-          repeat: -1,
-          yoyo: true,
-          ease: "none",
-          morphSVG: {
-            shape: walk3_paths[idx],
-            shapeIndex: shapeIndex,
-          },
-        }
-      );
-  });
+        const timeline = gsap.timeline({ repeat: -1, yoyo: true });
+        timeline
+            .fromTo(
+                item,
+                {
+                    morphSVG: walk_paths[idx],
+                },
+                {
+                    duration: 0.2,
+                    ease: 'none',
+                    morphSVG: {
+                        shape: walk2_paths[idx],
+                        shapeIndex: shapeIndex,
+                    },
+                }
+            )
+            .to(item, {
+                duration: 0.2,
+                ease: 'none',
+                morphSVG: {
+                    shape: walk3_paths[idx],
+                    shapeIndex: shapeIndex,
+                },
+            });
+    });
 }
